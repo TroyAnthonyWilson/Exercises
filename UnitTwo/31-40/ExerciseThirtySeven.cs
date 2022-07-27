dd5b37494c3f0308b780ab7ed30dc8e135f2332f
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-27 09:26:18 -0400

    finished exercise 37

---
+++ UnitTwo/31-40/ExerciseThirtySeven.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Exercises
+{
+    internal class ExerciseThirtySeven
+    {
+
+        public ExerciseThirtySeven()
+        {
+            Desc();
+            do
+            {
+
+                int[] intArray = new int[5];
+
+                for(int i = 0; i < intArray.Length; i++)
+                {
+                    intArray[i] = Tools.GetInt(0);
+                }
+
+                for(int i = 0; i < intArray.Length; i++)
+                {
+                    Console.Write($"{intArray[i]}");
+                    if(i < intArray.Length - 1)
+                    {
+                        Console.Write(" + ");
+                    }
+                }
+                Console.WriteLine($" = {intArray.Sum()}");
+
+            } while(Tools.IsYes());        
+        }
+
+
+        private static void Desc()
+        {
+            Description.DescriptionCall("Prompt the user to enter five numbers.  Store these numbers in an array and output their sum.");
+        }
+    }
+}
