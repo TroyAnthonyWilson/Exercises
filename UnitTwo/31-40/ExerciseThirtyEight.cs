c1f53ea7e3ee50afffa08e7280134c3cc62887f3
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-27 10:11:04 -0400

    Finished Exercise 38

---
+++ UnitTwo/31-40/ExerciseThirtyEight.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercises
+{
+    internal class ExerciseThirtyEight
+    {
+        public ExerciseThirtyEight()
+        {
+            Desc();
+            do
+            {
+
+                int[] intArray = new int[5];
+
+                for(int i = 0; i < intArray.Length; i++)
+                {
+                    intArray[i] = Tools.GetInt(-0);
+                }
+
+                for(int i = 0; i < intArray.Length; i++)
+                {
+                    Console.Write($"{intArray[i]}");
+                    if(i < intArray.Length - 1)
+                    {
+                        Console.Write(" + ");
+                    }
+                }
+                Console.WriteLine($" / {intArray.Length} = {intArray.Average()}");
+
+            } while(Tools.IsYes());
+        }
+
+        private static void Desc()
+        {
+            Description.DescriptionCall("Prompt the user to enter five numbers.  Store these numbers in an array and output their average.");
+        }
+    }
+}
