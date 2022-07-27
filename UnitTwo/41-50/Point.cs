3cfef65eb9ba94a572df7335e6cde560ced8f1fc
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-27 11:52:05 -0400

    Added CaculateDistance method

---
+++ UnitTwo/41-50/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercises
+{
+    internal class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public static double CalculateDistance(Point point)
+        {
+            return Math.Sqrt((Math.Pow(point.x, 2) + Math.Pow(point.y, 2)));
+        }
+
+    }
+}
