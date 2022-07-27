06b3f7003903fa58afd4ae6d92e74b012416527b
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-27 13:49:04 -0400

    added CalculateMidpoint

--- UnitTwo/41-50/Point.cs
+++ UnitTwo/41-50/Point.cs
@@ -2,12 +2,22 @@
 {
     internal class Point
     {
-        public int x { get; set; }
-        public int y { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
 
         public static double CalculateDistance(Point point)
         {
             return Math.Sqrt((Math.Pow(point.x, 2) + Math.Pow(point.y, 2)));
+        }
+
+        public static Point CalculateMidpoint(Point point1 , Point point2)
+        {
+            Point midpoint = new Point()
+            {
+                x = (point1.x + point2.x) / 2,
+                y = (point1.y + point2.y) / 2,
+            };
+            return midpoint;
         }
 
     }
