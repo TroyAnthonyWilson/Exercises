4a8b676e9cea80b5d532545820ca0691caf0fc3b
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-27 11:25:32 -0400

    Moved class point into it;s own class

---
+++ UnitTwo/41-50/ExerciseFourtyTwo.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercises
+{
+    internal class ExerciseFourtyTwo
+    {
+        public ExerciseFourtyTwo()
+        {
+            Desc();
+            do
+            {
+                Point point = new();
+                point.x = Tools.GetInt("Enter an X coordinate: ");
+                point.y = Tools.GetInt("Enter an Y coordinate: ");
+
+                Console.WriteLine($"You have created a point object ({point.x},{point.y})");
+
+            } while(Tools.IsYes());
+        }
+
+        private static void Desc()
+        {
+            Description.DescriptionCall("Create a class called Point, that has two properties, X and Y.\n" +
+                "Let the user enter an X and Y value to create a Point object.");
+        }
+    }
+
+
+}
