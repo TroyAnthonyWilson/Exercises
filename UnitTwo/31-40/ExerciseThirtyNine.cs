1add79782630fbbea5b2eb9503c5e9eea717e48f
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-27 10:20:47 -0400

    Finished Exercise 39

---
+++ UnitTwo/31-40/ExerciseThirtyNine.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercises
+{
+    internal class ExerciseThirtyNine
+    {
+        public ExerciseThirtyNine()
+        {
+            Desc();
+            do
+            {
+
+                int[] intArray = new int[5];
+
+                for(int i = 0; i < intArray.Length; i++)
+                {
+                    intArray[i] = Tools.GetInt(0);
+                }
+
+                intArray.OrderBy(x => x).ToList().ForEach(x => Console.Write($"{x} "));
+                Console.WriteLine();
+
+            } while(Tools.IsYes());
+        }
+
+        private static void Desc()
+        {
+            Description.DescriptionCall("Prompt the user to enter five numbers.  Store these numbers in an array and output them in ascending order.");
+        }
+    }
+}
