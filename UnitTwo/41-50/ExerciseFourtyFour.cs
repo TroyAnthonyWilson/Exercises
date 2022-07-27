b434e9ba5fe0bcc3301866598c93f0ff88749d3b
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-27 13:48:45 -0400

    Finished 44

---
+++ UnitTwo/41-50/ExerciseFourtyFour.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercises
+{
+    internal class ExerciseFourtyFour
+    {
+        public ExerciseFourtyFour()
+        {
+            Desc();
+            do
+            {
+                Point point1 = new()
+                {
+                    x = Tools.GetInt("Enter an X coordinate for the first point: "),
+                    y = Tools.GetInt("Enter an Y coordinate for the first point: ")
+                };
+                Point point2 = new()
+                {
+                    x = Tools.GetInt("Enter an X coordinate for the second point: "),
+                    y = Tools.GetInt("Enter an Y coordinate for the second point: ")
+                };
+                Point midpoint = Point.CalculateMidpoint(point1, point2);
+
+                Console.WriteLine($"The midpoint between ({point1.x},{point1.y}) and ({point2.x},{point2.y}) is ({midpoint.x},{midpoint.y}).");
+            } while(Tools.IsYes());
+        }
+
+        private static void Desc()
+        {
+            Description.DescriptionCall("Enhance the point class in the previous exercise by adding a CalculateMidpoint method./n" +
+                "The CalculateMidpoint will take as an argument another point instance\n" +
+                "and return the midpoint between the current ('this') object and the point passed in.\n" +
+                "Its method signature will look like: Point CalculateMidpoint(Point other) ");
+        }
+    }
+}
