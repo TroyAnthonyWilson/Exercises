4bb86fd9e4e2cf6c8096285170456fe9283a6f1e
Troy <wilson.troy.anthony@gmail.com>
2022-07-28 16:59:42 -0400

    Square class

---
+++ UnitTwo/41-50/Square.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Exercises
+{
+    internal class Square
+    {
+        private double _sideLength;
+        private double _perimeter;
+        private double _area;
+
+        public Square(double sideLenght)
+        {
+            _sideLength = sideLenght;
+            _perimeter = CalculatePerimeter();
+            _area = CalculateArea();
+            Console.WriteLine($"The square has side length {_sideLength}.  Its area is {_area} and its perimeter is {_perimeter}.");
+        }
+
+        private double CalculatePerimeter()
+        {
+            return _sideLength * 4;
+        }
+
+        private double CalculateArea()
+        {
+            return _sideLength * _sideLength;
+        }
+    }
+}
