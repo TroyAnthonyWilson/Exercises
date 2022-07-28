1e1c55c89e040aa0cdd5ae1ac1830f9739690f8f
Troy <wilson.troy.anthony@gmail.com>
2022-07-28 16:59:34 -0400

    Added exercise 45

---
+++ UnitTwo/41-50/ExerciseFourtyFive.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercises
+{
+    internal class ExerciseFourtyFive
+    {
+        public ExerciseFourtyFive()
+        {
+            Desc();
+
+            do
+            {
+                Square _ = new(Tools.GetDouble("Enter a side length: ", 0)); 
+            } while(Tools.IsYes());
+
+        }
+
+        private static void Desc()
+        {
+            Description.DescriptionCall("Create a class Square that has one property, SideLength." +
+                "\nThe class should have two methods, CalculatePerimeter, and CalculateArea. " +
+                "\nLet the user enter the length of the square's side. " +
+                "\nCreate an object based on that length and output details of the square.");
+        }
+    }
+}
