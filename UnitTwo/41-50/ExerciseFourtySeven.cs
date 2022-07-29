9fbb22e3c81d6daff0680aa10be10226587f0605
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-29 11:23:48 -0400

    Exercise 47

---
+++ UnitTwo/41-50/ExerciseFourtySeven.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercises
+{
+    internal class ExerciseFourtySeven
+    {
+        public ExerciseFourtySeven()
+        {
+            Desc();
+            List<string> words = new();
+            do
+            {
+                words.Add(Tools.GetWord("Please enter a word to add: "));
+                foreach(var item in words)
+                {
+                    Console.Write($"{item} ");
+                }
+                Console.WriteLine();
+            } while(Tools.IsYes());
+        }
+
+        private static void Desc()
+        {
+            Description.DescriptionCall("Repeatedly prompt the user to enter a string." +
+                "\nStore the string in a list and display the contents of the list with each element separated by a space.");
+        }
+    }
+}
