17b4ee415b55f939441a7e5237047853353b10b3
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-29 11:00:38 -0400

    Exercise 46

---
+++ UnitTwo/41-50/ExerciseFourtySix.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercises
+{
+    internal class ExerciseFourtySix
+    {
+        public ExerciseFourtySix()
+        {
+            Desc();
+
+            do
+            {
+                double[] sides = Tools.GetDoubleArray("Enter in side", 0, 3);
+                Triangle _ = new(sides);
+            } while(Tools.IsYes());
+        }
+
+    private static void Desc()
+    {
+        Description.DescriptionCall("Create a class Triangle, that has three properties, Side1Length, Side2Length, Side3Length" +
+            "\nThe class should have two methods, CalculatePerimeter, and CalculateArea. Let the user enter the length of the triangle's sides." +
+            "\nCreate an object based on those lengths and output the triangle's details. Hint: Google Heron's Formula.");
+    }
+}
+}
